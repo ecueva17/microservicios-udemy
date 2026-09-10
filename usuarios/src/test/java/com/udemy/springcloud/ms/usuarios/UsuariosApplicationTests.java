28718016c8cfa66e976c51d8fe95a079d935d6fb
Elvis Cueva <elvis.cueva.odar@gmail.com>
2026-09-10 15:02:20 -0500

    primer commit de proyecto usuarios

---
+++ usuarios/src/test/java/com/udemy/springcloud/ms/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springcloud.ms.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
